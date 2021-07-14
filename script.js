e9d694552a4dee5391f64d1295e0d56775b3dcae
nishkakotian <nishka308@gmail.com>
2021-07-14 17:20:55 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+Parse.initialize(
+    "RnGlQNbCqiZzPMNZC6ntIPYdo4dh9HspbP9R4QHF",
+    "4gJ7qW7gixxcegpLl52ye5fd7dsPHd49oQLmR9hs"
+);
+Parse.serverURL = 'https://pg-app-l1q9134ksi5ivcf6bu5yeuerlqpzaw.scalabl.cloud/1/';
+
+function register() {
+    var username = document.getElementById("username_reg").value;
+    var password = document.getElementById("pswd_reg").value;
+
+    var user = new Parse.User();
+    user.set("username", username);
+    user.set("password", password);
+
+    user.signUp().then(function success(user) {
+        window.location.replace("registered.html");
+    }, function error(err) {
+        console.log(err);
+    });
+}
+
+function login() {
+    var username = document.getElementById("username_login").value;
+    var password = document.getElementById("pswd_login").value;
+    Parse.User.logIn(username, password, { usePost: true }).then(function success(user) {
+        window.location.replace("owner.html");
+    }, function error(err) {
+        console.log(err);
+    });
+}
+
+function logout() {
+    Parse.User.logOut().then(function gotohome(user) {
+        window.location.replace("home.html");
+    });
+};
