61d4b75bf904b3ee7fcfab338ff32aa7faaa3dee
nishkakotian <nishka308@gmail.com>
2021-07-16 10:56:20 +0530

    Remove roles

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ Parse.serverURL = 'https://pg-app-l1q9134ksi5ivcf6bu5yeuerlqpzaw.scalabl.cloud/1
 
 //Function to toggle the type for password input element
 function visibilityToggle(pswdId, iconId) {
-    var pswdel = document.getElementById(pswdId);
-    var eyeicon = document.getElementById(iconId);
+    const pswdel = document.getElementById(pswdId);
+    const eyeicon = document.getElementById(iconId);
+
     if (pswdel.type == "password") {
         pswdel.type = "text";
         eyeicon.classList.add("fa-eye");
@@ -20,12 +21,12 @@ function visibilityToggle(pswdId, iconId) {
     }
 }
 
-function register() {
-    var username = document.getElementById("username_reg").value;
-    var password = document.getElementById("pswd_reg").value;
+function register(btnid) {
+    const username = document.getElementById("username_reg").value;
+    const password = document.getElementById("pswd_reg").value;
 
     if (password.length >= 8) {
-        var user = new Parse.User();
+        const user = new Parse.User();
         user.set("username", username);
         user.set("password", password);
 
@@ -38,13 +39,12 @@ function register() {
     else {
         document.getElementById("regError").innerHTML = "Password must be atleast 8 characters long";
     }
-
 }
 
 function login() {
     var username = document.getElementById("username_login").value;
     var password = document.getElementById("pswd_login").value;
-    Parse.User.logIn(username, password, { usePost: true }).then(function success(user) {
+    Parse.User.logIn(username, password, { usePost: true }).then(function success() {
         window.location.href = "owner.html";
     }, function error(err) {
         document.getElementById("loginError").innerHTML = err.message;
@@ -55,17 +55,4 @@ function logout() {
     Parse.User.logOut().then(function gotohome() {
         window.location.href = "home.html";
     });
-};
-
-//Run this function once to create the Roles for the application
-// function createRoles() {
-//     // By specifying no write privileges for the ACL, we can ensure the role cannot be altered.
-//     const roleACL = new Parse.ACL();
-//     roleACL.setPublicReadAccess(true);
-
-//     const ownerRole = new Parse.Role("Owner", roleACL);
-//     ownerRole.save();
-
-//     const customerRole = new Parse.Role("Customer", roleACL);
-//     customerRole.save();
-// }
+}
